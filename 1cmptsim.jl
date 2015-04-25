5236b309b23148a33d1293a5af889330de31c346
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-24 22:00:41 -0400

    clean up a bit

--- 1cmptsim.jl
+++ 1cmptsim.jl
@@ -1,22 +1,16 @@
-
 using Sundials
 include("regimen.jl")
 
-@everywhere function onecmptiv(t, a, adot, p)
+function onecmptiv(t, a, adot, p)
 	# p1 = KEL	
         adot[1] = -p[1]*a[1]
 end
-testing = [1., 2., 3.]
-testing
-testing2 = splice!(testing, 1:2)
-testing2
-testing
-@everywhere function sim_ind(regimen)
-p = [0.1] # KEL
-test_onecmpt(t, a, adot) = onecmptiv(t, a, adot, p)
+
+
+
+function sim_ind(f, regimen, sample_times)
 conc = Array(Float64,0)
 time = Array(Float64,0)
-sample_times = [0.:0.016:regimen[1][end]+24.]
 
 	for (j,amt) in enumerate(regimen[2])
 		dtime = (j == length(regimen[2]) ? sample_times[end] : regimen[1][j+1])
@@ -32,9 +26,9 @@ sample_times = [0.:0.016:regimen[1][end]+24.]
 			end
 		end
 		if j == 1	
-		y =Sundials.cvode(test_onecmpt, [amt], time_slice)
+		y =Sundials.cvode(f, [amt], time_slice)
 		else
-		y =Sundials.cvode(test_onecmpt, [conc[end] + amt], time_slice)
+		y =Sundials.cvode(f, [conc[end] + amt], time_slice)
 		end
 		append!(conc, y[1:end])
 		append!(time, time_slice)
@@ -42,19 +36,44 @@ sample_times = [0.:0.016:regimen[1][end]+24.]
 	end
 return (time, conc)
 end
-regimen(100., 5, interval = 12)
-sim_ind(regimen(100., 5, interval = 12))
+
+reg = regimen(100., 5., interval = 12)
+p = [0.1] # KEL
+test_onecmpt(t, a, adot) = onecmptiv(t, a, adot, p)
+sample_times = [0.:0.016:reg[1][end]+24.]
+
+sim_ind(test_onecmpt, reg, sample_times)
+
+#must redefine sampling times and such since modifying in place
+reg = regimen(100., 5., interval = 12)
+p = [0.1] # KEL
+test_onecmpt(t, a, adot) = onecmptiv(t, a, adot, p)
+sample_times = [0.:0.016:reg[1][end]+24.]
+@time sim =sim_ind(test_onecmpt, reg, sample_times)
+
+
+using DataFrames
+df = DataFrame(TIME = sim[1], CONC = sim[2])
+
 function all_sims()
  for i in 1:100
-		sim_ind(regimen(100., 5, interval = 12))
+reg = regimen(100., 5., interval = 12)
+p = [0.1] # KEL
+test_onecmpt(t, a, adot) = onecmptiv(t, a, adot, p)
+sample_times = [0.:0.016:reg[1][end]+24.]
+
+sim =sim_ind(test_onecmpt, reg, sample_times)
+
+df = DataFrame(TIME = sim[1], CONC = sim[2])
+df[:i] = i
+println(head(df))
 	end
 end
 all_sims()
-@time sim_ind(regimen(100., 5, interval = 12))
 @time all_sims()
 # need y is coming out as a 2dim array so this is hack to get it to 1dim so can be a df
-df = DataFrame(TIME = time, CONC = conc)
+
 # in order to take advantage of parallelization need to launch julia via
 # julia -p 4 (or however many processors on that comp)
-M = [regimen(100., 5, interval = 12)for i=1:100]
-@time pmap(sim_ind, M)
+#M = [regimen(100., 5, interval = 12)for i=1:100]
+#@time pmap(sim_ind, M)
