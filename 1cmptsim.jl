00e7bbe6f00325d22c2ec447836c570884abef05
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-24 21:10:01 -0400

    wrap in function and 100x sims for benchmarking quickly, will need to majorly refactor

--- 1cmptsim.jl
+++ 1cmptsim.jl
@@ -12,35 +12,47 @@ testing2 = splice!(testing, 1:2)
 testing2
 testing
 p = [0.1] # KEL
+function sim_ind(regimen)
 test_onecmpt(t, a, adot) = onecmptiv(t, a, adot, p)
 conc = Array(Float64,0)
 time = Array(Float64,0)
-sample_times = [0.:0.1:reg5_12[1][end]+24.]
-sample_times
-reg5_12 = regimen(100., 5, interval = 12)
-for (j,amt) in enumerate(reg5_12[2])
-	dtime = (j == length(reg5_12[2]) ? sample_times[end] : reg5_12[1][j+1])
-	
-	time_slice = Float64[] # must define outside for loop or time_slice only defined inside will not be available ever in global scope
-	for i in  1:length(sample_times) 
-		if sample_times[i] .< dtime 
-			continue
-		else 
-			time_slice = splice!(sample_times, 1:i-1) # as loop will go 1 past the last element we want only want to splice to i-1
-			push!(time_slice, dtime)
-			break
+sample_times = [0.:0.016:reg5_12[1][end]+24.]
+
+	for (j,amt) in enumerate(regimen[2])
+		dtime = (j == length(regimen[2]) ? sample_times[end] : reg5_12[1][j+1])
+		
+		time_slice = Float64[] # must define outside for loop or time_slice only defined inside will not be available ever in global scope
+		for i in  1:length(sample_times) 
+			if sample_times[i] .< dtime 
+				continue
+			else 
+				time_slice = splice!(sample_times, 1:i-1) # as loop will go 1 past the last element we want only want to splice to i-1
+				push!(time_slice, dtime)
+				break
+			end
 		end
+		if j == 1	
+		y =Sundials.cvode(test_onecmpt, [amt], time_slice)
+		else
+		y =Sundials.cvode(test_onecmpt, [conc[end] + amt], time_slice)
+		end
+		append!(conc, y[1:end])
+		append!(time, time_slice)
+		
 	end
-        if j == 1	
-	y =Sundials.cvode(test_onecmpt, [amt], time_slice)
-	else
-	y =Sundials.cvode(test_onecmpt, [conc[end] + amt], time_slice)
-	end
-	append!(conc, y[1:end])
-	append!(time, time_slice)
-	
+return (time, conc)
 end
-
-
+regimen(100., 5, interval = 12)
+sim_ind(regimen(100., 5, interval = 12))
+function all_sims()
+	for i in 1:100
+		sim_ind(regimen(100., 5, interval = 12))
+	end
+end
+@time sim_ind(regimen(100., 5, interval = 12))
+@time all_sims()
+# need y is coming out as a 2dim array so this is hack to get it to 1dim so can be a df
+df = DataFrame(TIME = time, CONC = conc)
+1/60
 
 
